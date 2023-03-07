5e932440b57a05ec42bf4f5dca35a27471b1679b
Ricky Mehra <107467908+RickyMehra29@users.noreply.github.com>
2023-03-07 20:33:52 +0530

    Update app.py
    Updated

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 # Load the model
 scalar = pickle.load(open("Red_Wine_Quality_scaler.pkl", "rb"))
-wine_model = pickle.load(open("Red_Wine_Quality_SVC.pkl", "rb"))
+wine_model = pickle.load(open("Red_Wine_Quality_model.pkl", "rb"))
+output_encoder = pickle.load(open("output_encoder.pkl", "rb"))
 
 @app.route('/')
 def home():
@@ -19,8 +20,10 @@ def predict_api():
     final_input = scalar.transform(np.array(data).reshape(1,-1))
     output = wine_model.predict(final_input)[0]
     print(output)
+    invert_output_encoder = dict(map(reversed, output_encoder.items()))
+    predicted_output = int((pd.Series(output).map(invert_output_encoder)).values)
     
-    return render_template('home.html', output_text="The Quality of Wine is {}.".format(output))
+    return render_template('home.html', output_text="The Quality of Wine is {}.".format(predicted_output))
 
 
 if __name__ == '__main__':
